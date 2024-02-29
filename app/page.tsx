22b60f7f3b662df96f2798407df05e1a32f62e0c
Roy Anger <roy@royanger.com>
2024-02-29 12:59:05 -0500

    Updated repo to match quickstart, updated to beta package

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <header>
       <SignedOut>
         <SignInButton />
-        <p>This content is public. Only signed out users can see this.</p>
       </SignedOut>
       <SignedIn>
         <UserButton />
-        <p>This content is private. Only signed in users can see this.</p>
       </SignedIn>
-    </div>
+    </header>
   )
 }
